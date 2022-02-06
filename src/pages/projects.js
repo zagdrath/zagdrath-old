5f01df69a6fe124a56c30b95cdff9b97c4b13b84
Cody Wellman <zagdrath@member.fsf.org>
2022-02-05 23:44:36 -0500

    Stuff

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,9 +17,6 @@ const ProjectsPage = () => {
       <article>
         <h2>Projects</h2>
         <p>This is a page for me to record my projects that I work on and a place for me to publish my blog articles that I write.</p>
-        <h3 class="project-list"><Link to='/projects/test'>Testing This Webpage</Link></h3>
-        <p class="project-list">Published: February 4, 2022</p>
-        <p class="project-list project-list-description">Stuff</p>
       </article>
       <Footer></Footer>
     </body>
