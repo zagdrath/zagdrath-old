b2d0e1cda7c985984305b4f8f52d35ed0403e31d
Cody Wellman <zagdrath@member.fsf.org>
2022-02-09 11:40:42 -0500

    Add link for WAN article

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -17,8 +17,9 @@ const WritingsPage = () => {
       <main>
         <h2>Writings</h2>
         <p>This is a page for my writings which may include blog posts that I write, tutorial or how-to guides, and documentation for any projects I may work on. I may also host mirrors of other's writings and etc.</p>
-        <p>To do:</p>
+        <p>To do: (I will get to writing these eventually)</p>
         <ul>
+          <li><Link to="/writings/setup-wan">How to Setup a WAN in Cisco Packet Tracer</Link>, a tutorial on how you would go about setting up a Wide Area Network(WAN) in Cisco Packet Tracer.</li>
           <li>Nokia IP440 Firewall</li>
           <li>Dell PowerVault MD1000</li>
           <li>How to Create Your own Tilde Server</li>
