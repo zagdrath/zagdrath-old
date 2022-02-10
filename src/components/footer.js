5582c0bee460dbd3ed0bfad848dbeefa25a8a705
Cody Wellman <zagdrath@member.fsf.org>
2022-02-10 16:11:20 -0500

    Edit footer again

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <footer>
         <hr></hr>
         <table width="100%" border="0">
-          <tr><a href='https://www.zagdrath.dev/'><em>Cody Wellman </em></a><a href='mailto:zagdrath@member.fsf.org'>&lt;<em>zagdrath@member.fsf.org</em>&gt;</a></tr>
+          <tr><a href='mailto:zagdrath@member.fsf.org'><em>Cody Wellman &lt;zagdrath@member.fsf.org&gt;</em></a></tr>
           <td>Copyright © 2022 Cody Wellman. All rights reserved.</td>
           <td align="right">Last updated February 10, 2022</td>
         </table>
