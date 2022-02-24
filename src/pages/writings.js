2df6868a26a869af276380d2c9887ad0e708327d
Cody Wellman <zagdrath@member.fsf.org>
2022-02-24 08:29:40 -0500

    Update

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -16,9 +16,9 @@ const WritingsPage = () => {
       <Header></Header>
       <main>
         <h2>Writings</h2>
-        <p>This is a page for my writings which may include blog posts that I write, tutorial or 
-        how-to guides, and documentation for any projects I may work on. I may also host 
-        mirrors of other's writings and etc.</p>
+        <p>This is a page for my writings which may include blog posts that I write, tutorial or
+          how-to guides, and documentation for any projects I may work on. I may also host
+          mirrors of other's writings and etc.</p>
         <p>These are blog posts, tutorials, and how-to guides that usually don't fall into the other categories on this page.</p>
         <ul>
           <li><Link to='/writings/setup-wan'>How to Setup a WAN in Cisco Packet Tracer</Link>, a tutorial on how you would go about setting up a Wide Area Network(WAN) in Cisco Packet Tracer.</li>
@@ -27,8 +27,8 @@ const WritingsPage = () => {
         <h2>Projects</h2>
         <p>Here are some projects that I either have worked on in the past or I am currently working on. Just as a warning some of these articles may be just me rambling.</p>
         <ul>
-          <li><Link to='/writings/nokiaip440'>Nokia IP440 Firewall</Link>, this is an old 4U firewall / security 
-          appliance I bought off of eBay, it happens to use a standard desktop Intel motherboard with a Pentium III.</li>
+          <li><Link to='/writings/nokiaip440'>Nokia IP440 Firewall</Link>, this is an old 4U firewall / security
+            appliance I bought off of eBay, it happens to use a standard desktop Intel motherboard with a Pentium III.</li>
           <li><Link to='/writings/dellmd1000'>Dell PowerVault MD1000</Link>, these are the chronicles of me trying to get a Dell PowerVault MD1000 to work properly, which has not been easy so far.</li>
           <li><Link to='/writings/babel'>The Library of Babel</Link>, this is a program that simulates The Library of Babel and is developed in Rust.</li>
           <li><Link to='/writings/retro'>Retro Networking</Link>, this is an attempt at building a 10BASE5 or a 10BASE2 network.</li>
@@ -36,9 +36,10 @@ const WritingsPage = () => {
         <h2>Books</h2>
         <p>These are some books that I highly recommend people to check out and read.</p>
         <ul>
-          <li><a href='../fsfs3-hardcover.pdf'>Free Software, Free Society</a>, this is the third edition of a selected 
-          collection of writings by Richard M. Stallman. It introduces the subject of history and development of the GNU 
-          Project and the Free Software Foundation. I also have the <a href='../rms-essays.pdf'>first edition</a> and <a href='../fsfs-ii-2.pdf'>second edition</a> available to download.</li>
+          <li><a href='../fsfs3-hardcover.pdf'>Free Software, Free Society</a>, this is the third edition of a selected
+            collection of writings by Richard M. Stallman. It introduces the subject of history and development of the GNU
+            Project and the Free Software Foundation. I also have the <a href='../rms-essays.pdf'>first edition</a> and <a href='../fsfs-ii-2.pdf'>second edition</a> available to download.</li>
+          <li><a href='temp'>At the Mountains of Madness</a> by H.P. Lovecraft</li>
         </ul>
       </main>
       <Footer></Footer>
--- src/pages/writings/retro.js
+++ src/pages/writings/retro.js
@@ -17,32 +17,33 @@ const Retro = () => {
       <Header></Header>
       <main>
         <h2>Retro Networking</h2>
-        <p>This is just an article about me trying to source all the materials to build a 10BASE5 
-        network. One of the most important pieces obviously is the cable, so I am currently 
-        trying to source some Belden 9880 cable for this network. My father may have some 
-        in a shed so I will be going to look to see if it is what I need.</p>
-        <p>Update February 18, 2022: I checked out the cable and the cable he was talking about ended up being 
-        TFC T-10, which is very similar to RG-6/U and is 75 Ohm rather than 50 Ohm RG-58A/U 
-        cable that I need for 10BASE2 networking. This cable obviously is not exactly correct 
-        for 10BASE2, but it should be able to work at a short distance for me until I am able 
-        to source some 10BASE5 or proper 10BASE2 cable for cheap. There also was a spool of 
-        RG-8, which is very similar to 10BASE5 cable but it ended up not being solid core, 
-        so I am unable to use it with the vamprire taps. I also bought a 3COM Etherlink 
-        XL PCI 3C900-COMBO card for my client systems and a Cabletron ST-500 transceiver 
-        that comes with an AMP 228752-1 vampire tap. Below are some images of the TFC 
-        T-10 cable and other parts I have gotten so far for this project. I also 
-        ordered some Klein Tools BNC connectors and a special crimper tool from them 
-        to put the ends on the cables.</p>
+        <p>This is just an article about me trying to source all the materials to build a 10BASE5
+          network. One of the most important pieces obviously is the cable, so I am currently
+          trying to source some Belden 9880 cable for this network. My father may have some
+          in a shed so I will be going to look to see if it is what I need.</p>
+        <p>Update February 18, 2022: I checked out the cable and the cable he was talking about ended up being
+          TFC T-10, which is very similar to RG-6/U and is 75 Ohm rather than 50 Ohm RG-58A/U
+          cable that I need for 10BASE2 networking. This cable obviously is not exactly correct
+          for 10BASE2, but it should be able to work at a short distance for me until I am able
+          to source some 10BASE5 or proper 10BASE2 cable for cheap. There also was a spool of
+          RG-8, which is very similar to 10BASE5 cable but it ended up not being solid core,
+          so I am unable to use it with the vamprire taps. I also bought a 3COM Etherlink
+          XL PCI 3C900-COMBO card for my client systems and a Cabletron ST-500 transceiver
+          that comes with an AMP 228752-1 vampire tap. Below are some images of the TFC
+          T-10 cable and other parts I have gotten so far for this project. I also
+          ordered some Klein Tools BNC connectors and a special crimper tool from them
+          to put the ends on the cables.</p>
+        <p>Update February 23, 2022: I recieved the PCI card and I still need to install it in my Sun Ultra 20 to test it and take some pictures of it. The Klein Tools compression crimper and ends also came today, so I made one cable as a test. The ST-500 transceiver is actually coming from India and I did not realize that, so it will be awhile before it arrives. But in the meantime, I need to order some BNC T connectors and terminators.</p>
         <table width="100%" border="0">
           <tr>
             <td width="33%" class="table-align">
-              <StaticImage src='../../images/t10.png' alt='TFC T-10 Cable'/>
+              <StaticImage src='../../images/t10.png' alt='TFC T-10 Cable' />
             </td>
             <td width="33%" class="table-align">
-              <StaticImage src='../../images/3com.png' alt='3COM Etherlink XL PCI 3C900-COMBO'/>
+              <StaticImage src='../../images/3com.png' alt='3COM Etherlink XL PCI 3C900-COMBO' />
             </td>
             <td width="33%" class="table-align">
-              <StaticImage src='../../images/st500.png' alt='Cabletron ST-500'/>
+              <StaticImage src='../../images/st500.png' alt='Cabletron ST-500' />
             </td>
           </tr>
         </table>
